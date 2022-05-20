4f2a27e14b9e73f44e72028a5d5031011bda3f82
G.Mohan Teja <mohanteja.g2019@vitstudent.ac.in>
2022-05-20 16:47:07 +0530

    delete images

--- preprocess.m
+++ preprocess.m
@@ -1,8 +1,12 @@
-function resize(a,b,directory,to,ext)
+function preprocess(a,b,directory,to,ext)
     srcFiles = dir(strcat(directory,filesep,'*.',ext));
     for i = 1 : length(srcFiles)
         filename = strcat(directory,filesep,srcFiles(i).name);
         im = imread(filename);
+        [m,n,c] = size(im);
+        if c>1
+            im = rgb2gray(im);
+        end
         k=imresize(im,[a,b]);
         nm = strcat(to,'/',srcFiles(i).name);
         imwrite(k,nm,ext);
